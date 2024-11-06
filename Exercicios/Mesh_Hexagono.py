6a331a69e9d04e40ef49361e89f27d3efb4343bf
nniicks <jhonathanduartebarreto@gmail.com>
2024-11-06 10:04:21 -0400

    mesh hexagono

--- Exercicios/Mesh_Hexagono.py
+++ Exercicios/Mesh_Hexagono.py
@@ -7,24 +7,34 @@ import math
 # create variables used in the loop
 radius_step = 0.1
 current_radius = 0.1
-number_triangles = 50
+number_hexagons = 50
 
-for i in range(number_triangles):
+z_step = 10
+
+for i in range(1, number_hexagons):
 
     # add a triangle mesh into the scene
     current_radius = current_radius + radius_step;  # raio atual
-    bpy.ops.mesh.primitive_circle_add(vertices=3, radius=current_radius)
-    # OBS: para mudar a figura que sera criada basta mudar a quantidade de vertices 
-
+    bpy.ops.mesh.primitive_circle_add(vertices=6, radius=current_radius)
+    
 
     # get a reference to the currently active object
-    triangle_mesh = bpy.context.active_object
+    hexagons_mesh = bpy.context.active_object
 
     # rotate mesh about the x-axis
     #triangle_mesh.rotation_euler.x = math.radians(-90)
     graus = -90
     radianos = math.radians(graus)
-    triangle_mesh.rotation_euler.x = radianos
+    hexagons_mesh.rotation_euler.x = radianos
+    
+    # rotate mesh about the z-axis
+    #triangle_mesh.rotation_euler.z = math.radians(ang em graus)
+    graus = z_step * i
+    radianos = math.radians(graus)
+    hexagons_mesh.rotation_euler.z = radianos
+    
+    
+
 
     # convert mesh into a curve (convertendo a malha para uma curva)
     bpy.ops.object.convert(target='CURVE')
@@ -32,8 +42,8 @@ for i in range(number_triangles):
     # add a bevel to curve
     #bpy.context.object.data.bevel_depth = 0.12
     #bpy.context.object.data.bevel_resolution = 16
-    triangle_mesh.data.bevel_depth = 0.05
-    triangle_mesh.data.bevel_resolution = 16
+    hexagons_mesh.data.bevel_depth = 0.05
+    hexagons_mesh.data.bevel_resolution = 16
 
     # shade smooth (suavizando a sombra)
     bpy.ops.object.shade_smooth()
