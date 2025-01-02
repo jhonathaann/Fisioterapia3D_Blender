655b9f2897e81c6094434665b7a680cab81cbaa5
nniicks <jhonathanduartebarreto@gmail.com>
2025-01-01 21:26:20 -0400

    correções no calculo do perimetro da circunferencia

--- Códigos/perimetro_circunferencia.py
+++ Códigos/perimetro_circunferencia.py
@@ -8,11 +8,12 @@ if obj and obj.type == 'MESH':
     # acessando a malha do objeto
     mesh = obj.data
     
-    quant_vertices = len(obj.data.vertices)
+    quant_vert = len(obj.data.vertices)
 
     coord_x = []
     coord_y = []
     coord_z = []
+    
     
     for vert in mesh.vertices:
         
@@ -21,7 +22,7 @@ if obj and obj.type == 'MESH':
         coord_z.append(vert.co.z) # salvando as coord de z
     
     perimetro = 0 
-    for i in range(quant_vertices-1):
+    for i in range(quant_vert-1):
         
         # pegando as coord de um ponto
         x1,y1,z1 = coord_x[i], coord_y[i], coord_z[i]
@@ -30,7 +31,14 @@ if obj and obj.type == 'MESH':
         x2,y2,z2 = coord_x[i+1], coord_y[i+1], coord_z[i+1]
         
         perimetro += math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+        
+    # calculando a distancia entre o primeiro e o ultimo ponto (para fechar a volta
+    # inteira na circunferencia)
+    x1,y1,z1 = coord_x[0], coord_y[0], coord_z[0]
+    x2,y2,z2 = coord_x[quant_vert-1], coord_y[quant_vert-1], coord_z[quant_vert-1]
     
+    perimetro += math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+        
     print(f"perimetro do objeto {obj.name} : {perimetro}")
 else:
     print("nao foi selecionando um objeto do tipo MESH")
